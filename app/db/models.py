1b032757bccd30128cab62db4a3c33885a843c08
vivekashok1221 <vivekashok1221@gmail.com>
2023-07-05 03:58:17 +0530

    Implemented signup and token generation endpoints

--- app/db/models.py
+++ app/db/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from uuid import UUID, uuid4
 
 from sqlalchemy import DateTime, ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID as pg_UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
@@ -11,7 +13,11 @@ class User(Base):
 
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(
+        pg_UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
@@ -29,10 +35,14 @@ class Link(Base):
 
     __tablename__ = "link"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(
+        pg_UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
     original_url: Mapped[str]
     short_url: Mapped[str] = mapped_column(unique=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 
     user: Mapped["User"] = relationship(back_populates="links")
@@ -43,8 +53,12 @@ class Token(Base):
 
     __tablename__ = "token"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    id: Mapped[UUID] = mapped_column(
+        pg_UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("user.id"))
     token: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 
